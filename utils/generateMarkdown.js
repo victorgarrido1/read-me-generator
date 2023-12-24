7ab19320a4135cf99be48534cbac5f506ebda3ec
victorgarrido1 <vcgarr94@gmail.com>
2023-12-23 21:37:42 -0700

    adding links of licensces

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,11 +9,43 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let renderLicenseLink = '';
+
+  switch(license) {
+    case 'MIT':
+      licenseLink = "https://mit-license.org/";
+      break;
+    case "ZPL":
+      licenseLink = "https://opensource.org/license/zpl-2-1/";
+      case "Mozilla":
+        licenseLink = "https://opensource.org/license/mpl-2-0/";
+        case 'GNU':
+          licenseLink = "https://opensource.org/license/lgpl-3-0/";
+          case: 
+          licenseLink = "http://www.apache.org/licenses/LICENSE-2.0";
+          break;
+          default:
+            licenseLink = "";
+            break;
+  }
+
+
+  return licenseLink;
+  //select the correct license link for the selected license
+  
+  
+  }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ''; // if no license is provided. it will return a empty string
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
